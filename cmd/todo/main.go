e8222e14de56d0cf7c6070620e3619697929021b
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2021-10-10 17:13:07 -0500

    removed strings package and added flag package

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/RedHoodJT1988/todo-cli"
 )
@@ -12,9 +12,25 @@ import (
 const todoFileName = ".todo.json"
 
 func main() {
+	// Testing out the Usage function
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+			"%s tool. Developed for a convienent way to create tasks of ToDo Items|n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Copyright 2021\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage information:")
+		flag.PrintDefaults()
+	}
+	// Parsing command line flags
+	task := flag.String("task", "", "Task to be included in the ToDo List")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+
+	flag.Parse()
+
+	// Define an item list
 	l := &todo.List{}
 	
-	// Use the Get method to read ToDo items from file
+	// Use the Get command to read ToDo items from file
 	if err := l.Get(todoFileName); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
@@ -22,6 +38,36 @@ func main() {
 
 	// Decide what to do based on the number of arguments provided
 	switch {
+	case *list:
+		// List current to do items
+		for _, item := range *l {
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
+		}
+	case *complete > 0:
+		// Complete the Given item
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		
+	case *task != "":
+		// Add the task
+		l.Add(*task)
+
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
 		// For no extra arguments, print the list
 	case len(os.Args) == 1:
 		for _, item := range *l {
@@ -30,14 +76,8 @@ func main() {
 		// Concatenate all provided arguments with a space and 
 		// add to the list as an item
 	default:
-		// Concatenate all arguments with a space
-		item := strings.Join(os.Args[1:], " ")
-		l.Add(item)
-
-		// Save the new list
-		if err := l.Save(todoFileName); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
+		// Invalid flag provided
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
 	}
 }
